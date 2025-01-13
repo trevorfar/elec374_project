670d2a2be701f3199cbabf04c23f1f6503cdb2a9
Trevor Farias <levelonethousandr@gmail.com>
2025-01-12 22:37:37 -0500

    adding first files, 32bit adder orsk here

---
+++ adder_32_bit_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module adder_32_bit_tb;
+
+reg [31:0] a;
+reg [31:0] b;
+reg cin;
+wire [31:0] sum;
+wire cout;
+
+adder_32_bit uut(.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+initial begin
+		  a = 32'b0100_0100_0100_0100_0100_0100; b = 32'b0100_0100_0100_0100_0100_0100; cin = 1'b0;
+		  #50;
+		
+		 $stop;
+end
+endmodule
+
+
