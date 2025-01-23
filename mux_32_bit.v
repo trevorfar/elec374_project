e3ae523b1aa0b4cd67990dc9d4dfce45d4a2e3f9
Trevor Farias <levelonethousandr@gmail.com>
2025-01-23 15:32:38 -0500

    good

--- mux_32_bit.v
+++ mux_32_bit.v
@@ -1,4 +1,4 @@
-odule mux_32_bit(
+module mux_32_bit(
 	input [31:0] R0, R1, R2, R3, R4, R5, R6, R7, 
 	R8, R9, R10, R11, R12, R13, R14, R15,
 	HI, LO, Z_HI, Z_LO, PC, MDR, IN_PORT, C_sign_extended,
