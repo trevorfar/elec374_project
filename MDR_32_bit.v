11fbfbb1ae724ce3efd08692808a02109f7eada9
Trevor Farias <21tof1@queensu.ca>
2025-01-27 12:35:22 -0500

    mdr works

--- MDR_32_bit.v
+++ MDR_32_bit.v
@@ -2,15 +2,24 @@ module mdr_32_bit(
 	input [31:0] Mdatain,
 	input [31:0] bus_mux_out,
 	input wire clk, clear, MDRin, select,
-	output wire [31:0] mdr_out
+	output reg [31:0] mdr_out
 );
 	wire [31:0] D;
 	
 	mux_2_to_1 MDRmux(.input0(bus_mux_out), .input1(Mdatain), .select(select), .mux_output(D));
+	
+	always @(posedge clk) begin
+		if(clear) begin
+			mdr_out <= 32'b0;
+		end else if (MDRin) begin
+			mdr_out <= D;
+		end
 
-	reg_32_bit MDR(
-		.clk(clk), .clear(clear), .enable(MDRin), .BusMuxOut(D), .BusMuxIn(mdr_out)
-	);
+	end
+	
+// pretty sure I could do this reg_32_bit MDR(
+//		.clk(clk), .clear(clear), .enable(MDRin), .BusMuxOut(D), .BusMuxIn(mdr_out)
+//	);
 
 
 endmodule
