27f68901f0b5f50438e2a45af9f48d9b2fad270d
trevor <trevor_farias@yahoo.com>
2025-02-09 13:50:00 -0500

    changed div algo

--- div_32_bit.v
+++ div_32_bit.v
@@ -1,32 +1,78 @@
 module div_32_bit(
-	input [31:0] dividend,
-	input [31:0] divisor,
-	output reg [31:0] quotient,
-	output reg [31:0] remainder
-);
-	reg [63:0] remainder_reg;
-	reg [5:0] counter;
-	
-	integer i;
-	
-	always @(*) begin
-		remainder_reg = {32'b0, dividend};
-		quotient = {32{1'b0}};
-		
-		for(i = 0; i < 32; i = i + 1) begin
-			remainder_reg = remainder_reg << 1;
-			remainder_reg[63:32] = remainder_reg[63:32] - divisor;
+    
+    input [31:0] Q,
+    input [31:0] M,
+    input clk, resetn,
+	 
+    output [31:0] quotient,
+    output [31:0] remainder
 
-			if(remainder_reg[63]) begin // neg
-				remainder_reg[63:32] = remainder_reg[63:32] + divisor;
-				remainder_reg[0] = 1'b0;
-				
-			end else begin
-				remainder_reg[0] = 1'b1;
+);
+    reg [63:0] AQ_reg;
+    integer count;	 
+
+	always @(posedge clk) begin
+		
+		if (~resetn) begin
+		
+			AQ_reg = 64'b0;
+			count = 0;
+		
+		end
+		
+		else if (count == 0) begin
+			
+			count = count + 1;
+			
+			AQ_reg = {32'b0, Q};
+			
+		end
+		
+		else if (count >= 1 && count <= 32) begin
+		
+			count = count + 1;
+			AQ_reg = AQ_reg << 1;
+			
+			if (AQ_reg[63] == 1'b0) begin
+				AQ_reg[63:32] = AQ_reg[63:32] - M;
+			end
+			else begin
+				AQ_reg[63:32] = AQ_reg[63:32] + M;
 			end
 			
-		quotient = remainder_reg[31:0];
-		remainder = remainder_reg[63:32];
+			AQ_reg[0] = (AQ_reg[63] == 1'b0) ? 1'b1 : 1'b0;
+		
 		end
+		
+		else if (AQ_reg[63] == 1) begin
+		
+			AQ_reg[63:32] = AQ_reg[63:32] + M;
+			
+		end
+		
 	end
+
+	assign quotient = AQ_reg[31:0];
+   assign remainder = AQ_reg[63:32];	
+
 endmodule
+
+
+
+
+
+
+	
+		
+		
+	
+    
+
+
+   
+
+    
+
+    
+
+    
--- div_32_bit_tb.v
+++ div_32_bit_tb.v
@@ -1,40 +1,85 @@
-`timescale 1ns / 1ps
 
 module div_32_bit_tb;
-
-    reg [31:0] dividend;
-    reg [31:0] divisor;
+    reg [31:0] Q;
+    reg [31:0] M;
+    reg clk, resetn;
     wire [31:0] quotient;
     wire [31:0] remainder;
 
     div_32_bit uut (
-        .dividend(dividend),
-        .divisor(divisor),
+        .Q(Q),
+        .M(M),
+        .clk(clk),
+		  .resetn(resetn),
         .quotient(quotient),
         .remainder(remainder)
     );
-
-    initial begin
-        dividend = 32'b1000_0000_0000_0000_0000_0000_0000; 
-        divisor = 32'b0000_1000_0000_0000_0000_0000_0000; 
-        #50;
-
-        dividend = 32'd250; 
-        divisor = 32'd25; 
-        #50;
-
-        dividend = 32'd100; 
-        divisor = 32'd30; 
-        #50;
-
-        dividend = 32'd5; 
-        divisor = 32'd10; 
-        #50;
-
-        dividend = 32'd12345; 
-        divisor = 32'd1; 
-        #50;
-
-        $stop; 
+    always begin
+	     clk = 0;
+        forever #5 clk = ~clk;
     end
-endmodule
+
+	initial begin
+		  
+		resetn = 0;
+
+		@ (posedge clk)
+
+		resetn = 1;
+		Q = 32'd38;  
+		M = 32'd6;   
+
+		#340;  
+
+		@ (posedge clk)
+		
+		resetn = 0;
+
+		@ (posedge clk)
+
+		resetn = 1;
+		Q = 32'd100;
+		M = 32'd25;
+		#340;
+		
+		@ (posedge clk)
+		
+		resetn = 0;
+
+		@ (posedge clk)
+
+		resetn = 1;
+		Q = {0,{31{1'b1}}};
+		M = {0,{31{1'b1}}};
+		#340;
+		
+		@ (posedge clk)
+		
+		resetn = 0;
+		
+		@ (posedge clk)
+
+		resetn = 1;
+		Q = {0,{31{1'b1}}};
+		M = 32'b1;
+		#340;
+		
+		@ (posedge clk)
+		
+		resetn = 0;
+		
+		@ (posedge clk)
+
+		resetn = 1;
+		Q = 32'b1;
+		M = 32'd50;
+		#340;
+		
+		@ (posedge clk)
+		
+		resetn = 0;
+		
+		@ (posedge clk)
+		$stop;
+	end
+	endmodule
