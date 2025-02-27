9f097a46058a3eb88151b07953f13261780ab46a
Trevor Farias <21tof1@queensu.ca>
2025-02-26 22:00:09 -0500

    load reg probably works, foster says it looks good!

--- datapath_ld_tb.v
+++ datapath_ld_tb.v
@@ -5,6 +5,7 @@ module datapath_ld_tb();
 
     reg clk, clear;
     wire [4:0] opcode;
+	 reg [31:0] inport_data_in;
     reg pc_out, ZLowout, HI_out, LO_out, mar_in, pc_in, ZHighout, muxy_select, inc_pc, Rin;
     reg mdr_in, ir_in, Yin, mdr_read, HI_in, LO_in, z_lo_in, z_hi_in, Cout, mdr_out, rz_in, inport_out, inport_in, outport_in, Gra, Grb, Grc, wren, BAout, Rout;
 	 wire [15:0] reg_in, reg_out; 
@@ -19,17 +20,11 @@ module datapath_ld_tb();
 	 ldi R4, 0x54
 	 ldi R6, 0x63(R2) ; R2 = 0x78
 	 */
-//	 
-//    parameter Default = 4'b0000, load_regA1 = 4'b0001, load_regA2 = 4'b0010, T0 = 4'b0011, T1 = 4'b0100, T2 = 4'b0101, T3 = 4'b0110,
-//				  T4 = 4'b0111, T5 = 4'b1000, T6 = 4'b1001, T7 = 4'b1010;
-//				  
-//    reg [3:0] present_state, next_state;
-	 
+
 	 datapath DUT 	(
     .clk(clk),
     .clear(clear),
-    .inport_data_in(),  
-    .outport_data_out(),  // Unconnected output
+    .outport_data_out(),  
     .bus_data(bus_data), 
 	 .ram_data_out(ram_data_out),
 	 .ir_data_out(ir_data_out),
@@ -70,7 +65,8 @@ module datapath_ld_tb();
 	 .Rin(Rin),
 	 .pc_data_out(pc_data_out),
 	 .Mdatain(Mdatain),
-	 .Rout(Rout)
+	 .Rout(Rout),
+	 .inport_data_in(inport_data_in)
 	);
 	
 	 initial begin
@@ -117,6 +113,23 @@ module datapath_ld_tb();
 		
         end
     endtask
+
+	task load_reg (input [31:0] opcode, input [31:0] value); begin
+		inport_data_in <= opcode; inport_in <= 1;
+		@(posedge clk)
+		inport_data_in <= value; inport_in <= 1;
+		inport_out <= 1; ir_in <= 1;
+		
+		@(posedge clk)
+		inport_data_in <= 32'b0; inport_in <= 0;
+		inport_out <= 1; Gra <= 1; Rin <= 1;
+		
+		@(posedge clk)
+		inport_out <= 0; Gra <= 0; Rin <= 0;
+	
+	end
+	endtask
+	 
 	 
 	 task T0(); begin
 		pc_out <= 1; mar_in <= 1; inc_pc <= 1; pc_in <= 1;
@@ -140,6 +153,7 @@ module datapath_ld_tb();
 	 endtask
 	 
 	 task T3(); begin
+		
 	 
 	 end
 	 endtask
@@ -163,61 +177,44 @@ module datapath_ld_tb();
 	 
 	 end
 	 endtask
-	 
+	
+
 	 initial begin
 		reset_signals();
 		
 		@(posedge clk)
+		load_reg(32'hB180000, 32'h22);
 		T0();
 		T1();
-		T2();
-		T0();
-		T1();
-		T2();
-		
-		
-//		T3();
-//		T4();
-//		T5();
-//		T6();
-//		T7();
+		T2();		
+		T3();
+		T4();
+		T5();
+		T6();
+		T7();
 	 end
 
+	 /*
+	 task T0(); begin
+		pc_out <= 1; mar_in <= 1; inc_pc <= 1; pc_in <= 1;
+		@(posedge clk) 
+		pc_out <= 0; mar_in <= 0; inc_pc <= 0; pc_in <= 0;
+	 end
+	 endtask
+	 
+	 task T1(); begin
+		mdr_read <= 1; mdr_in <= 1; 
+		@(posedge clk)
+		mdr_read <= 0; mdr_in <= 0; 
+	 end
+	 endtask
+	 
+	 task T2(); begin
+		mdr_out <= 1; ir_in <= 1; 
+		@(posedge clk)
+		mdr_out <= 0; ir_in <= 0;
+	 end
+	 endtask
+	 */
+	 
 endmodule
-	 
-	 
-//	 task load_regA1(); begin
-//	   mdr_read <= 1; mdr_in <= 1; pc_out <= 1; mar_in <= 1; 
-//		@(posedge clk) 
-//		mdr_read <= 0; mdr_in <= 0; pc_out <= 0; mar_out <= 0;
-//
-//	 end
-//	 endtask
-//	 
-//	 task load_regA2(); begin
-//	 	ir_in <= 1;
-//	 @(posedge clk)
-//		ir_in <= 0;
-//	 end
-//	 endtask
-//	 
-//	 task load_regA3(); begin
-//		pc_in <= 1; inc_pc <= 1;
-//		@(posedge clk)
-//	 	pc_in <= 0; inc_pc <= 0; 
-//	 end
-//	 endtask
-//	 
-//	 task load_regA4(); begin
-//		 mdr_in <= 1; mdr_read <= 1;
-//		 @(posedge clk)
-//		 mdr_in <= 0; mdr_read <= 0;
-//	 end
-//	 endtask
-//	 
-//	 task load_regA5(); begin
-//		 mdr_out <= 1;
-//		 @(posedge clk)
-//		 mdr_out <= 0;
-//	 end
-//	 endtask
